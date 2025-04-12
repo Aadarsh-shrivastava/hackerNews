8405bafd83359239ef8f91ae4cb8b134aac4455c
Aadarsh_shrivastava <53793726+Aadarsh-shrivastava@users.noreply.github.com>
2025-04-12 16:33:28 +0530

    setup: home route and layout

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,18 @@
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Feed } from "../../components/Feed/Feed";
 import "./HomePage.css";
 
 export const Homepage = () => {
   return (
     <div className="homepage-container">
       <Header />
-      <div className="homepage-body"></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Feed />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );

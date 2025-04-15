f2b39633f3d0cfb20a8bb52bc1f9d0ca811ef88e
aadarsh-shrivastava <aadarshkhurai@gmail.com>
2025-04-15 19:08:59 +0530

    Merge remote-tracking branch 'origin/main' into feature/unit-test-setup

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,12 @@
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  HashRouter,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import { Feed } from "../../components/Feed/Feed";
 import "./HomePage.css";
 
@@ -8,11 +14,17 @@ export const Homepage = () => {
   return (
     <div className="homepage-container">
       <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Feed />} />
-        </Routes>
-      </BrowserRouter>
+      <div className="homepage-body">
+        <HashRouter>
+          <Routes>
+            {/* Redirect root path to /new */}
+            <Route path="/" element={<Navigate to="/new" replace />} />
+
+            {/* Dynamic feedtype route */}
+            <Route path="/:feedtype" element={<Feed />} />
+          </Routes>
+        </HashRouter>
+      </div>
       <Footer />
     </div>
   );

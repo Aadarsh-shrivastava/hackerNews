c6871fba5c7d88fb86705ef37b3051bf43b5d1e8
Aadarsh_shrivastava <53793726+Aadarsh-shrivastava@users.noreply.github.com>
2025-04-15 17:15:00 +0530

    chore: add error state in hook to display errors
    * chore: add error state in hook t display error
    
    * fix: custom error message
    
    * fix: error message
    
    * refactor: remove catch block from init call

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -12,13 +12,20 @@ import { useNavigate, useParams } from "react-router-dom";
 export const Feed = () => {
   const { feedtype } = useParams();
   const navigate = useNavigate();
-  const { stories, loading, loadMore, hasMore } = useStories(
-    `${feedtype}stories`
+  const { stories, loading, loadMore, hasMore, errors } = useStories(
+    `${feedtype}`
   );
 
+  if (!loading && errors.length) {
+    return (
+      <div className="feed-container">
+        <span className="fallback-message">{errors[0].message}</span>
+      </div>
+    );
+  }
   return (
     <div className="feed-container">
-      {/* list of cswitchable tab chips */}
+      {/* list of switchable tab chips */}
       <div className="chips-container">
         {storyTypeChips.map((chip) => (
           <Chip
@@ -55,4 +62,5 @@ export const Feed = () => {
         />
       )}
     </div>
- );
+  );
+};

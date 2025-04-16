dad18cab7136ac4a823281c5270f21dcf85a4cbf
aadarsh-shrivastava <aadarshkhurai@gmail.com>
2025-04-16 12:26:17 +0530

    test: add a test to check wrong url

--- cypress/e2e/loadStoriesFlow.cy.js
+++ cypress/e2e/loadStoriesFlow.cy.js
@@ -4,13 +4,13 @@ describe('News Feed App', () => {
 
     cy.get('[data-testid="loading-indicator"]').should('be.visible');
 
-    cy.get('[data-testid^="news-card"]').should('have.length', 10);
+    cy.get('[data-testid^="news-card"]').should('have.length', 5);
 
     cy.get('[data-testid="loading-indicator"]').should('not.exist');
 
     cy.contains('Load More').click();
 
-    cy.get('[data-testid^="news-card"]').should('have.length', 20);
+    cy.get('[data-testid^="news-card"]').should('have.length', 10);
   });
 
   it('opens story link in new tab', () => {
@@ -21,5 +21,11 @@ describe('News Feed App', () => {
 
       cy.request(href).its('status').should('eq', 200); // ensure the link works
     });
+  });
+
+  it('displays error when going to wrong url', () => {
+    cy.visit('http://localhost:5173/#/trending');
+
+    cy.contains('The page you are looking for does not exist. Please go back to the homepage.').should('be.visible')
   });
 });

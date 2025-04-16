d75e2432ce537a3a48e1f97d0d13e36a6be5e103
aadarsh-shrivastava <aadarshkhurai@gmail.com>
2025-04-16 14:15:50 +0530

    fix: chnage number of stories in test

--- cypress/e2e/switchTabFlow.cy.js
+++ cypress/e2e/switchTabFlow.cy.js
@@ -1,5 +1,5 @@
 describe('Switch Tab FLow', () => {
-    it('loads new 10 stories by default on main page', () => {
+    it('loads new 5 stories by default on main page', () => {
         cy.intercept('GET', '**/v0/newstories.json').as('getStoryIds');
         cy.intercept('GET', '**/v0/item/*.json').as('getStories');
 
@@ -14,7 +14,7 @@ describe('Switch Tab FLow', () => {
         cy.get('[data-testid^="news-card"]').should('have.length', 5);
     });
 
-    it('loads top 10 stories when switched to top tab', () => {
+    it('loads top 5 stories when switched to top tab', () => {
         cy.intercept('GET', '**/v0/topstories.json').as('getTopStoryIds');
         cy.intercept('GET', '**/v0/item/*.json').as('getStories');
 
